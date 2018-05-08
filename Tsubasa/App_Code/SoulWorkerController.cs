12b514763cecc7195b35e8b14120a210ef0bde46
Vanessa White <assenavmai@users.noreply.github.com>
2018-05-07 23:24:11 -0400

    Issue 19: Application Enhancements
    Adding questions for part of issue 19
    (Character level, discord, youtube and twitch)

--- Tsubasa/App_Code/SoulWorkerController.cs
+++ Tsubasa/App_Code/SoulWorkerController.cs
@@ -46,16 +46,20 @@ public class SoulWorkerController : Controller
             //Build String to send to Discord Webhook
             string application = "```ini\n[" + Request["Q1"] + "]\n" + model.CharacterName +
             "\n\n[" + Request["Q2"] + "]\n" + model.Class +
-            "\n\n[" + Request["Q3"] + "]\n" + model.Location +
-            "\n\n[" + Request["Q4"] + "]\n" + model.Experience +
-            "\n\n[" + Request["Q5"] + "]\n" + model.Preference +
-            "\n\n[" + Request["Q6"] + "]\n" + model.Voice +
-            "\n\n[" + Request["Q7"] + "]\n" + model.Playtime +
-            "\n\n[" + Request["Q8"] + "]\n" + model.PlayerType +
-            "\n\n[" + Request["Q9"] + "]\n" + model.PrevGuilds +
-            "\n\n[" + Request["Q10"] + "]\n" + model.LookingFor +
-            "\n\n[" + Request["Q11"] + "]\n" + model.PrevMMOs +
-            "\n\n[" + Request["Q12"] + "]\n" + model.Contribute + "```\n";
+            "\n\n[" + Request["Q3"] + "]\n" + model.CharacterLevel +
+            "\n\n[" + Request["Q4"] + "]\n" + model.Location +
+            "\n\n[" + Request["Q5"] + "]\n" + model.Experience +
+            "\n\n[" + Request["Q6"] + "]\n" + model.Preference +
+            "\n\n[" + Request["Q7"] + "]\n" + model.Voice +
+            "\n\n[" + Request["Q8"] + "]\n" + model.Playtime +
+            "\n\n[" + Request["Q9"] + "]\n" + model.PlayerType +
+            "\n\n[" + Request["Q10"] + "]\n" + model.PrevGuilds +
+            "\n\n[" + Request["Q11"] + "]\n" + model.LookingFor +
+            "\n\n[" + Request["Q12"] + "]\n" + model.PrevMMOs +
+            "\n\n[" + Request["Q13"] + "]\n" + model.Contribute +
+            "\n\n[" + Request["Q14"] + "]\n" + model.DiscordHandle +
+            "\n\n[" + Request["Q15"] + "]\n" + model.TwitchHandle +
+            "\n\n[" + Request["Q16"] + "]\n" + model.YouTubeHandle + "```\n";
 
             /*Build XML to put in Answers column, Q[n] are questions from 
             hidden inputs to store what the questions were at the time in DB */
@@ -66,34 +70,46 @@ public class SoulWorkerController : Controller
                     model.Class),
                 new XElement("Answer",
                     new XAttribute("Question", Request["Q3"]),
-                    model.Location),
+                    model.CharacterLevel),
                 new XElement("Answer",
                     new XAttribute("Question", Request["Q4"]),
-                    model.Experience),
+                    model.Location),
                 new XElement("Answer",
                     new XAttribute("Question", Request["Q5"]),
-                    model.Preference),
+                    model.Experience),
                 new XElement("Answer",
                     new XAttribute("Question", Request["Q6"]),
-                    model.Voice),
+                    model.Preference),
                 new XElement("Answer",
                     new XAttribute("Question", Request["Q7"]),
-                    model.Playtime),
+                    model.Voice),
                 new XElement("Answer",
                     new XAttribute("Question", Request["Q8"]),
-                    model.PlayerType),
+                    model.Playtime),
                 new XElement("Answer",
                     new XAttribute("Question", Request["Q9"]),
-                    model.PrevGuilds),
+                    model.PlayerType),
                 new XElement("Answer",
                     new XAttribute("Question", Request["Q10"]),
-                    model.LookingFor),
+                    model.PrevGuilds),
                 new XElement("Answer",
                     new XAttribute("Question", Request["Q11"]),
-                    model.PrevMMOs),
+                    model.LookingFor),
                 new XElement("Answer",
                     new XAttribute("Question", Request["Q12"]),
-                    model.Contribute));
+                    model.PrevMMOs),
+                new XElement("Answer",
+                    new XAttribute("Question", Request["Q13"]),
+                    model.Contribute),
+            new XElement("Answer",
+                    new XAttribute("Question", Request["Q14"]),
+                    model.DiscordHandle),
+            new XElement("Answer",
+                    new XAttribute("Question", Request["Q15"]),
+                    model.TwitchHandle),
+            new XElement("Answer",
+                    new XAttribute("Question", Request["Q16"]),
+                    model.YouTubeHandle));
 
             //If Captcha is valid, proceed
             bool validCaptcha = ReCaptcha.ValidateCaptcha(userResponse);
