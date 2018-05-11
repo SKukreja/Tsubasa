72634c75373220be2b75823d64f025981044c68a
SKukreja <sumit.kukreja@live.com>
2018-05-11 00:00:16 -0400

    Updated page layout and working role authentication

--- Tsubasa/App_Code/Controllers/HomeController.cs
+++ Tsubasa/App_Code/Controllers/HomeController.cs
@@ -1,18 +1,45 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity.Owin;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Tsubasa.Models;
 
 /// <summary>
 /// Summary description for HomeController
 /// </summary>
-namespace Tsubasa
+namespace Tsubasa.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationUserManager _userManager;
+
+        public ApplicationUserManager UserManager
+        {
+            get
+            {
+                return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
+        }
+
         public ActionResult Default()
         {
+            if (Request.IsAuthenticated)
+            {
+                return RedirectToAction("Home", "Home");
+            }
+            return View();
+        }
+
+        public ActionResult Home()
+        {
+            ViewData["UserManager"] = UserManager;
             return View();
         }
     }
--- Tsubasa/App_Code/Startup/RouteConfig.cs
+++ Tsubasa/App_Code/Startup/RouteConfig.cs
@@ -14,6 +14,12 @@ namespace Tsubasa
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "Home",
+                url: "Home",
+                defaults: new { controller = "Home", action = "Home", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Default", id = UrlParameter.Optional }
